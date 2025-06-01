c1f3f49d886b6ae72bb3574ce5cf876ade95fd08
cinnamon <mizuki.minase@westerlund.co.jp>
2025-06-01 16:33:38 +0900

    google form added

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as motion from 'framer-motion/client';
 import { HomePage } from '@/types/strapi';
 import { FaLine } from 'react-icons/fa';
+import { FaFeatherPointed } from 'react-icons/fa6';
 
 interface ContactSectionProps {
   homeData: HomePage;
@@ -17,14 +18,23 @@ const ContactSection: React.FC<ContactSectionProps> = ({ homeData }) => {
         </div>
 
         <div className="fl fdc po-c">
-          <div className='fl jcc mb7'>以下のLINEからご相談内容をお送りください。</div>
+          <div className='fl jcc mb7'>以下の Google Form または LINEからご相談内容をお送りください。</div>
           <motion.div
-            className='w100 px4 mxa fl jcc'
+            className='w100 px4 mxa fl fdc jcc'
             initial={{ opacity: 0, x: -30 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8 }}
             viewport={{ once: true }}
           >
+            <a
+              href="https://forms.gle/pNTUCqF5ZbKGyJzPA"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="fl aic g2 link hover:opacity-80 transition-opacity fs2xl mb4"
+            >
+              <FaFeatherPointed className="text-4xl" />
+              <span className="text-lg font-medium">Form から送信</span>
+            </a>
             <a
               href="https://lin.ee/PpUlbVA"
               target="_blank"
