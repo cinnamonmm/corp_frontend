4ba7e02326e5044d4c6becfbdcd2d6ce8cbd7ad4
cinnamon <mizuki.minase@westerlund.co.jp>
2025-06-02 19:32:07 +0900

    next config updated

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', '127.0.0.1', '**.westerlund.co.jp', '**.strapiapp.com'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.westerlund.co.jp',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.strapiapp.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
